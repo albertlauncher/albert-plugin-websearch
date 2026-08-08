870e40ed0082040a29d980349ff1d72e276179d5
Manuel Schneider <manuelschneid3r@gmail.com>
2026-08-08 16:39:41 +0200

    feat: Add italian  translations

---
+++ i18n/websearch_it.ts
@@ -0,0 +1,136 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>ConfigWidget</name>
+    <message>
+        <source>Double click to edit.</source>
+        <translation>Doppio clic per modificare.</translation>
+    </message>
+    <message>
+        <source>Add</source>
+        <translation>Aggiungi</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>Rimuovi</translation>
+    </message>
+    <message>
+        <source>Restore</source>
+        <translation>Ripristina</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation>Nome</translation>
+    </message>
+    <message>
+        <source>Name of the search engine.</source>
+        <translation>Nome del motore di ricerca.</translation>
+    </message>
+    <message>
+        <source>Short</source>
+        <translation>Breve</translation>
+    </message>
+    <message>
+        <source>Short name you can utilize for quick access.</source>
+        <translation>Nome breve utilizzabile per l&apos;accesso rapido.</translation>
+    </message>
+    <message>
+        <source>URL</source>
+        <translation>URL</translation>
+    </message>
+    <message>
+        <source>The URL of this search engine. %s will be replaced by your search term.</source>
+        <translation>L&apos;URL di questo motore di ricerca. %s verrà sostituito dal termine di ricerca.</translation>
+    </message>
+    <message>
+        <source>F</source>
+        <comment>Short for Fallback</comment>
+        <translation>F</translation>
+    </message>
+    <message>
+        <source>Enable as fallback.</source>
+        <translation>Abilita come fallback.</translation>
+    </message>
+    <message>
+        <source>Could not save image to &apos;%1&apos;.</source>
+        <translation>Impossibile salvare l&apos;immagine in &apos;%1&apos;.</translation>
+    </message>
+    <message>
+        <source>Do you really want to remove &apos;%1&apos; from the search engines?</source>
+        <translation>Vuoi davvero rimuovere &apos;%1&apos; dai motori di ricerca?</translation>
+    </message>
+    <message>
+        <source>Do you really want to restore the default search engines?</source>
+        <translation>Vuoi davvero ripristinare i motori di ricerca predefiniti?</translation>
+    </message>
+</context>
+<context>
+    <name>Plugin</name>
+    <message>
+        <source>Search %1 for &apos;%2&apos;</source>
+        <translation>Cerca %1 per &apos;%2&apos;</translation>
+    </message>
+    <message>
+        <source>Run websearch</source>
+        <translation>Esegui ricerca web</translation>
+    </message>
+</context>
+<context>
+    <name>SearchEngineEditor</name>
+    <message>
+        <source>Edit search engine</source>
+        <translation>Modifica motore di ricerca</translation>
+    </message>
+    <message>
+        <source>Click to set an icon for the search engine.</source>
+        <translation>Clicca per impostare un&apos;icona per il motore di ricerca.</translation>
+    </message>
+    <message>
+        <source>Click to select file
+or drop an image.</source>
+        <translation>Clicca per selezionare un file
+o trascina un&apos;immagine.</translation>
+    </message>
+    <message>
+        <source>Name:</source>
+        <translation>Nome:</translation>
+    </message>
+    <message>
+        <source>Name of the search engine.</source>
+        <translation>Nome del motore di ricerca.</translation>
+    </message>
+    <message>
+        <source>Short:</source>
+        <translation>Breve:</translation>
+    </message>
+    <message>
+        <source>Short name you can utilize for quick access.</source>
+        <translation>Nome breve utilizzabile per l&apos;accesso rapido.</translation>
+    </message>
+    <message>
+        <source>URL:</source>
+        <translation>URL:</translation>
+    </message>
+    <message>
+        <source>The URL containing a %s that will be replaced by the query.</source>
+        <translation>L&apos;URL contenente un %s che verrà sostituito dalla query.</translation>
+    </message>
+    <message>
+        <source>Fallback:</source>
+        <translation>Fallback:</translation>
+    </message>
+    <message>
+        <source>Enable this search engine as fallback item.</source>
+        <translation>Abilita questo motore di ricerca come elemento di fallback.</translation>
+    </message>
+    <message>
+        <source>Choose icon</source>
+        <translation>Scegli icona</translation>
+    </message>
+    <message>
+        <source>Images (*.png *.svg)</source>
+        <translation>Immagini (*.png *.svg)</translation>
+    </message>
+</context>
+</TS>
